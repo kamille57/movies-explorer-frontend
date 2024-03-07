6d6eeb69a4aaad980828a16a6a8532738e6bf060
kamille57 <126716411+kamille57@users.noreply.github.com>
2024-03-07 15:01:07 +0300

    добавить раздельное состояние для сохраненных и несохраненных фильмов компоненту чекбокс короткометражек

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React from 'react'; 
 
-function FilterCheckbox({ setOnlyShortMovies, onlyShortMovies }) {
-  const handleCheckboxChange = () => {
-    localStorage.setItem('onlyShortMovies', !onlyShortMovies)
-    setOnlyShortMovies(!onlyShortMovies);
-  };
+function FilterCheckbox({ setOnlyShortMovies, onlyShortMovies, isSaved }) { 
+  const handleCheckboxChange = () => { 
+    const storageKey = isSaved ? 'savedOnlyShortMovies' : 'moviesOnlyShortMovies';
+    localStorage.setItem(storageKey, !onlyShortMovies); 
+    setOnlyShortMovies(!onlyShortMovies); 
+  }; 
 
-  return (
-    <section className="filter">
-      <form>
-        <label className="filter-checkbox" htmlFor="checkbox">
-          <input
-            className="filter-checkbox__input"
-            type="checkbox"
-            id="checkbox"
-            checked={onlyShortMovies}
-            onChange={handleCheckboxChange}
-          />
-          <span className="filter-checkbox__inner"></span> Короткометражки
-        </label>
-      </form>
-    </section>
-  );
-}
+  return ( 
+    <section className="filter"> 
+      <form> 
+        <label className="filter-checkbox" htmlFor="checkbox"> 
+          <input 
+            className="filter-checkbox__input" 
+            type="checkbox" 
+            id="checkbox" 
+            checked={onlyShortMovies} 
+            onChange={handleCheckboxChange} 
+          /> 
+          <span className="filter-checkbox__inner"></span> Короткометражки 
+        </label> 
+      </form> 
+    </section> 
+  ); 
+} 
 
 export default FilterCheckbox;
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from 'react'; 
-import FilterCheckbox from "../FilterCheckbox/FilterCheckbox.js"; 
+import React, { useState, useEffect } from "react";
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox.js";
 
-function SearchForm({ cards, handleSearch, isSaved }) { 
-  const [searchQuery, setSearchQuery] = useState(() => 
-    localStorage.getItem(isSaved ? 'savedMoviesSearchQuery' : 'moviesSearchQuery') || ""
-  ); 
+function SearchForm({ cards, handleSearch, isSaved }) {
+  const [searchQuery, setSearchQuery] = useState(
+    () =>
+      localStorage.getItem(
+        isSaved ? "savedMoviesSearchQuery" : "moviesSearchQuery"
+      ) || ""
+  );
   const [onlyShortMovies, setOnlyShortMovies] = useState(
-    localStorage.getItem(isSaved ? 'savedOnlyShortMovies' : 'moviesOnlyShortMovies') === 'true' || false
-  ); 
+    localStorage.getItem(
+      isSaved ? "savedOnlyShortMovies" : "moviesOnlyShortMovies"
+    ) === "true" || false
+  );
 
   useEffect(() => {
     handleFilteredResults(searchQuery);
@@ -15,24 +20,27 @@ function SearchForm({ cards, handleSearch, isSaved }) {
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
-  }
+  };
 
   const handleFilteredResults = (searchQuery) => {
-    const regex = new RegExp(searchQuery, 'gi');
-    let filtered = cards.filter(card => card.nameRU.match(regex));
+    const regex = new RegExp(searchQuery, "gi");
+    let filtered = cards.filter((card) => card.nameRU.match(regex));
 
     if (onlyShortMovies) {
-      filtered = filtered.filter(card => card.duration <= 40);
+      filtered = filtered.filter((card) => card.duration <= 40);
     }
 
     handleSearch(filtered);
-    localStorage.setItem(isSaved ? 'savedMoviesSearchQuery' : 'moviesSearchQuery', searchQuery);
-  }
+    localStorage.setItem(
+      isSaved ? "savedMoviesSearchQuery" : "moviesSearchQuery",
+      searchQuery
+    );
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     handleFilteredResults(searchQuery);
-  }
+  };
 
   return (
     <section className="search">
@@ -44,9 +52,17 @@ function SearchForm({ cards, handleSearch, isSaved }) {
           value={searchQuery}
           onChange={handleChange}
         />
-        <button type="submit" className="search__btn" aria-label="Кнопка запроса"></button>
+        <button
+          type="submit"
+          className="search__btn"
+          aria-label="Кнопка запроса"
+        ></button>
       </form>
-      <FilterCheckbox setOnlyShortMovies={setOnlyShortMovies} onlyShortMovies={onlyShortMovies} />
+      <FilterCheckbox
+        setOnlyShortMovies={setOnlyShortMovies}
+        onlyShortMovies={onlyShortMovies}
+        isSaved={isSaved}
+      />
     </section>
   );
 }
