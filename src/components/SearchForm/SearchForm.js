6708e9743357a7b900467b01fffe603b30f7d0d8
kamille57 <126716411+kamille57@users.noreply.github.com>
2024-03-10 22:11:38 +0300

    добавить ошибку в валидацию инпута searchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from "react";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox.js";
 
 function SearchForm({ cards, handleSearch, isSaved, getAllMovies }) {
-  const [searchQuery, setSearchQuery] = useState(localStorage.getItem("moviesSearchQuery") || ''); 
+  const [searchQuery, setSearchQuery] = useState(
+    localStorage.getItem("moviesSearchQuery") || ""
+  );
   const [onlyShortMovies, setOnlyShortMovies] = useState(
     localStorage.getItem(
       isSaved ? "savedOnlyShortMovies" : "moviesOnlyShortMovies"
     ) === "true" || false
   );
-
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
   const handleFilteredResults = async () => {
-    console.log('handleFilteredResults');
-    if(cards.length === 0 && !isSaved) {
+    console.log("handleFilteredResults");
+    if (cards.length === 0 && !isSaved) {
       console.warn("Вы передаёте пустые фильмы, вам надо скачать их");
       localStorage.setItem("moviesSearchQuery", searchQuery);
       await getAllMovies();
@@ -28,38 +30,42 @@ function SearchForm({ cards, handleSearch, isSaved, getAllMovies }) {
     let filtered = cards.filter((card) => card.nameRU.match(regex));
 
     if (onlyShortMovies) {
-        filtered = filtered.filter((card) => card.duration <= 40);
+      filtered = filtered.filter((card) => card.duration <= 40);
     }
 
     handleSearch(filtered);
 
     if (!isSaved) {
-        localStorage.setItem("moviesSearchQuery", searchQuery);
+      localStorage.setItem("moviesSearchQuery", searchQuery);
     }
-};
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-      try {
-        handleFilteredResults(searchQuery);
-
-      } catch (error) {
-        console.error("Error fetching movies from the server", error);
-      }
-    
+    if (searchQuery.trim() === "") {
+      console.log('я тут');
+      setErrorMessage("Нужно ввести ключевое слово");
+      return;
+    }
+    try {
+      handleFilteredResults(searchQuery);
+      setErrorMessage("");
+    } catch (error) {
+      console.error("Error fetching movies from the server", error);
+      setErrorMessage("Ошибка при поиске фильмов");
+    }
   };
 
   useEffect(() => {
-    console.log('Карточки поменялись');
+    console.log("Карточки поменялись");
     console.log(cards.length);
     if (cards.length === 0) {
-        return;
+      return;
     }
     if (searchQuery) {
-        handleFilteredResults();
+      handleFilteredResults();
     }
-}, [cards, onlyShortMovies]);
+  }, [cards, onlyShortMovies]);
 
   return (
     <section className="search">
@@ -67,8 +73,8 @@ function SearchForm({ cards, handleSearch, isSaved, getAllMovies }) {
         <input
           type="text"
           placeholder="Фильм"
-          className="search-input"
-          value={isSaved ? '' : searchQuery}
+          className={`search-input ${errorMessage && "search-input_error"}`}
+          value={isSaved ? "" : searchQuery}
           onChange={handleChange}
         />
         <button
@@ -77,6 +83,9 @@ function SearchForm({ cards, handleSearch, isSaved, getAllMovies }) {
           aria-label="Кнопка запроса"
         ></button>
       </form>
+      <span className={`search__error-message ${errorMessage && "search__error-message_active"}`} 
+>{errorMessage}</span> 
+
       <FilterCheckbox
         setOnlyShortMovies={setOnlyShortMovies}
         onlyShortMovies={onlyShortMovies}
