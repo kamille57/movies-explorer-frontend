d0b80de3c9ec65ef7ac1805817e6b2984905a351
kamille57 <126716411+kamille57@users.noreply.github.com>
2024-03-16 11:27:37 +0300

    исправить, чтобы кнопка ЕЩЕ была только в блоке MOVIES

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import MoviesCard from "../MoviesCard/MoviesCard.js";
-import { VISIBLE_MOVIES, WINDOW_WIDTH_THRESHOLD, FILMS_TO_LOAD_MORE } from "../../constants/constants.js";
+import {
+  VISIBLE_MOVIES,
+  WINDOW_WIDTH_THRESHOLD,
+  FILMS_TO_LOAD_MORE,
+} from "../../constants/constants.js";
 
 function MoviesCardList({
   cards,
@@ -33,11 +37,20 @@ function MoviesCardList({
 
   useEffect(() => {
     if (windowWidth >= WINDOW_WIDTH_THRESHOLD.LARGE) {
-      changeMovieListOptions(VISIBLE_MOVIES.LARGE, FILMS_TO_LOAD_MORE.FULL_SCREEN);
+      changeMovieListOptions(
+        VISIBLE_MOVIES.LARGE,
+        FILMS_TO_LOAD_MORE.FULL_SCREEN
+      );
     } else if (windowWidth >= WINDOW_WIDTH_THRESHOLD.SMALL) {
-      changeMovieListOptions(VISIBLE_MOVIES.SMALL, FILMS_TO_LOAD_MORE.SMALL_SCREEN);
+      changeMovieListOptions(
+        VISIBLE_MOVIES.SMALL,
+        FILMS_TO_LOAD_MORE.SMALL_SCREEN
+      );
     } else {
-      changeMovieListOptions(VISIBLE_MOVIES.EXTRA_SMALL, FILMS_TO_LOAD_MORE.SMALL_SCREEN);
+      changeMovieListOptions(
+        VISIBLE_MOVIES.EXTRA_SMALL,
+        FILMS_TO_LOAD_MORE.SMALL_SCREEN
+      );
     }
   }, [windowWidth]);
 
@@ -58,40 +71,54 @@ function MoviesCardList({
 
   return (
     <>
-      {(serverMessage && (searchQuery === "" || savedSearchQuery === "")) ||
-      (cards.length === 0 && (!likedMovies || likedMovies.length !== 0) && isSaved) ||
-      (cards.length === 0 && !isSaved && initialMovies.length !== 0) ? (
+      {serverMessage &&
+      (searchQuery === "" || savedSearchQuery === "") &&
+      ((cards.length === 0 &&
+        (!likedMovies || likedMovies.length !== 0) &&
+        isSaved) ||
+        (cards.length === 0 && !isSaved && initialMovies)) ? (
         <h3 className="movies__empty-request">Ничего не найдено</h3>
-      ) : (initialMovies === "" && searchQuery === "" && !isSaved) ||
-        (initialMovies === "" && !isSaved) ? (
+      ) : (!initialMovies && searchQuery === "" && !isSaved) ||
+        (!initialMovies && !isSaved) ? (
         <div className="movies__empty-request"></div>
-      ) : ((likedMovies.length === 0 || !likedMovies) && isSaved) ? (
+      ) : (!likedMovies || likedMovies.length === 0) && isSaved ? (
         <div className="movies__empty-request"> Сохраненных фильмов нет.</div>
       ) : (
-        <section className="cards"> 
-        <ul className="cards__container"> 
-          {cards && cards.slice(0, cardsLimit).map((newCard) => ( 
-            <li key={newCard.id}> 
-              <MoviesCard 
-                card={newCard} 
-                handleLike={handleLike} 
-                handleDelete={handleDelete} 
-                isSaved={isSaved} 
-              /> 
-            </li> 
-          ))} 
-        </ul> 
-        {cards.length > cardsLimit && ( 
-          <button 
-            type="button" 
-            aria-label="кнопка для показа большего количества фильмов" 
-            className="cards__btn" 
-            onClick={addMoreItems} 
-          > 
-            Ещё 
-          </button> 
-        )} 
-      </section>
+        <section className="cards">
+          <ul className="cards__container">
+            {isSaved
+              ? cards.map((newCard) => (
+                  <li key={newCard.id}>
+                    <MoviesCard
+                      card={newCard}
+                      handleLike={handleLike}
+                      handleDelete={handleDelete}
+                      isSaved={isSaved}
+                    />
+                  </li>
+                ))
+              : cards && cards.slice(0, cardsLimit).map((newCard) => (
+                  <li key={newCard.id}>
+                    <MoviesCard
+                      card={newCard}
+                      handleLike={handleLike}
+                      handleDelete={handleDelete}
+                      isSaved={isSaved}
+                    />
+                  </li>
+                ))}
+          </ul>
+          {cards.length > cardsLimit && !isSaved && (
+            <button
+              type="button"
+              aria-label="кнопка для показа большего количества фильмов"
+              className="cards__btn"
+              onClick={addMoreItems}
+            >
+              Ещё
+            </button>
+          )}
+        </section>
       )}
     </>
   );
